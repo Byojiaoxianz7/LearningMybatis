71ab624dacf7f2bbdf53d9308e9b09823f0cd9d8
byojiaoxianz7 <byaojiaoxianz7@outlook.com>
2020-03-21 14:41:56 +0800

    使用注解进行增删改查, MybatisUtils中自动提交事务, 使用 lombok 插件

--- mybatis-05/src/main/java/com/pojo/User.java
+++ mybatis-05/src/main/java/com/pojo/User.java
@@ -7,56 +7,21 @@ package com.pojo;
  * @Github: https://github.com/byojiaoxianz7
  */
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
- * 实体类
+ * 实体类使用lombok插件
  * @author Waciao
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private Integer id;
     private String name;
     private String pwd;
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + pwd + '\'' +
-                '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return pwd;
-    }
-
-    public void setPassword(String password) {
-        this.pwd = password;
-    }
-
-    public User(Integer id) {
-        this.id = id;
-    }
-
-    public User(Integer id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.pwd = password;
-    }
 }
